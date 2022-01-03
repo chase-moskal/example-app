16e9e61ffa696f907b7098558d9b2dc83d9af479
Chase Moskal <chasemoskal@gmail.com>
2022-01-02 16:58:43 -0800

    fix: iife for top-level await

--- s/main.ts
+++ s/main.ts
@@ -4,13 +4,17 @@ console.log("🤖 hello")
 import {html, render} from "lit"
 import {exampleRemote} from "./api/example-remote.js"
 
-// lit demo
-const element = document.querySelector<HTMLDivElement>(".app")
-if (element) {
-	const content = html`<p>(lit-rendered content)</p>`
-	render(content, element)
-}
+void async function() {
 
-// renraku api demo
-const {calculator} = exampleRemote("http://localhost:8000/")
-console.log("sum of 2 + 3 =", await calculator.sum(2, 3))
+	// lit demo
+	const element = document.querySelector<HTMLDivElement>(".app")
+	if (element) {
+		const content = html`<p>(lit-rendered content)</p>`
+		render(content, element)
+	}
+
+	// renraku api demo
+	const {calculator} = exampleRemote("http://localhost:8000/")
+	console.log("sum of 2 + 3 =", await calculator.sum(2, 3))
+
+}()
